27521ba28521db3a4889eb5947539972e92680fa
Nicho Reyes <reyes.nicho@gmail.com>
2020-07-25 23:15:01 +0800

    work history

--- src/app/main-layout/main-layout.component.ts
+++ src/app/main-layout/main-layout.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-main-layout',
-  templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.scss']
+    selector: 'app-main-layout',
+    templateUrl: './main-layout.component.html',
+    styleUrls: ['./main-layout.component.scss']
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+    oqas = 'Outsourced';
+    tworks = 'Teloworks';
+    tm = 'Trend Micro';
+    smartmatic = 'Smartmatic';
+    sds = 'SD Solutions';
 
-  ngOnInit(): void {
-  }
+    activeTab: string;
 
+    ngOnInit(): void {
+        this.activeTab = this.oqas;
+    }
+
+    tabMenuItemClick(event, item: string): void {
+        event.preventDefault();
+        event.stopPropagation();
+
+        this.activeTab = item;
+    }
+
+    tabActive(item: string) {
+        return this.activeTab === item;
+    }
 }
