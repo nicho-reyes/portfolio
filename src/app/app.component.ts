9a09ebd396f2fbd46fd0225cd582b60b837cef37
Nicho Reyes <reyes.nicho@gmail.com>
2020-07-25 22:21:32 +0800

    added some stuffs on about section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    title = 'Nicho Reyes | Software Web Developer';
+    title = 'Nicho Reyes | Software Developer';
 }
